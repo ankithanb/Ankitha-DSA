a27a73e25dd81c0770a87d44f200252bdec90db9
Ankitha <nbankitha3878@gmail.com>
2026-03-14 19:23:14 +0530

    Added RightRotation.java file

---
+++ array/RightRotation.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.ArrayList;
+public class RightRotation {
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
+        ArrayList<Integer> List = new ArrayList<>();
+        for(int i = k; i < arr.length; i++)
+        {
+            List.add(arr[i]);
+        }
+        for(int i = 0; i < k; i++)
+        {
+            List.add(arr[i]);
+        }
+        System.out.println(List);
+    }
+}
