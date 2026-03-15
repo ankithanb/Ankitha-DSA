a3612ad62cf950564b400eb194b91d2bbc98bd05
Ankitha <nbankitha3878@gmail.com>
2026-03-15 13:04:33 +0530

    File updated

---
+++ array/LongestConseSeq.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class LongestConseSeq {
+    public static void main(String[] args)
+    {
+        int[] arr = {100,2,0,3,4,6,9,1};
+        int min = Integer.MAX_VALUE;
+        System.out.println(min);
+    }
+}
