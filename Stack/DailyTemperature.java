3778f213a4624ffe9ef9f3f76859a13ca06e244b
Ankitha <nbankitha3878@gmail.com>
2026-04-16 19:36:04 +0530

    Files added

---
+++ Stack/DailyTemperature.java
@@ -0,0 +1,60 @@
+// this is a program to find How many days to wait for a warmer temperature.
+
+// Algorithm
+// Traverse from right → left
+// For each index i:
+//      Remove all smaller or equal temperatures
+//      If stack empty → 0
+//      Else → stack.peek() - i
+//      Push current index
+
+// Time complexity is O(n) (for Stack) where traverse through each element in an given array.
+// Space Complexity is O(n).
+
+
+
+package Stack;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DailyTemperature {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int[] res = Find(arr);
+        System.out.println(Arrays.toString(res));
+        sc.close();
+    }
+
+    static int[] Find(int[] arr)
+    {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) 
+            {
+                stack.pop();
+            }
+            if(stack.isEmpty())
+            {
+                res[i] = 0;
+            }
+            else{
+                res[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
---
+++ Stack/PreviousGreater.java
@@ -0,0 +1,59 @@
+// this is a program to find the Previous Greater Element.
+
+// Algorithm
+// Travers from right to left.
+// For each element :
+//      Remove elements which are smaller than the current element in the stack.
+//      if the stack is empty, then push -1.
+//      else push the stack.peek() element.
+//      push the current element into stack.
+
+// Time complexity is O(n) (for Stack) where traverse through each element in an given array.
+// Space Complexity is O(n).
+
+package Stack;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PreviousGreater {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int[] res = Find(arr);
+        System.out.println(Arrays.toString(res));
+        sc.close();
+    }
+
+    static int[] Find(int[] arr)
+    {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++)
+        {
+            while(!stack.isEmpty() && stack.peek() <= arr[i])
+            {
+                stack.pop();
+            }
+            if(stack.isEmpty())
+            {
+                res[i] = -1;
+            }
+            else{
+                res[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+}
